5062e8fe6d884c555ba5e6e99f4716454299a6fd
Brian Burns <bdburns6389@gmail.com>
2018-08-12 19:43:30 -0500

    added ByteShortInt project

---
+++ java/ByteShortInt/src/com/BrianBurns/Main.java
@@ -0,0 +1,11 @@
+package com.BrianBurns;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myMinValue = -2147483648;  //smallest possible int value
+        int myMinValueUnderscore = -2_147_483_648;  // same as above, underscores for readability
+        int myMaxValue = 2147483647;  //largest possible int value
+
+    }
+}
