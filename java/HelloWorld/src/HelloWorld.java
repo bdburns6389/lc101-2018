16f833a2bb69eddcfa7a36f33acb0b8e51cd7158
Brian Burns <bdburns6389@gmail.com>
2018-08-12 19:09:17 -0500

    initial commit

---
+++ java/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        String name = "brian"; //Creates string variable
+        int age = 28; // creates integer variable
+        System.out.println("Hello World!" + name + age); //prints out Hello World and name variable
+    }
+}
